cedd31493b865e54e2abc485cdf43632813e508c
cainmagi <cainmagi@gmail.com>
2022-04-25 15:10:40 -0500

    4/25/2022 - 2
    1. Adjust the styles of the code blocks.
    2. (experimental, dropped) try to make experiments about line numbers.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-const lightCodeTheme = require('prism-react-renderer/themes/vsLight');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 const math = require('remark-math');
 const katex = require('rehype-katex');
